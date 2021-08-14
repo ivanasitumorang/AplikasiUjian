f8b0ce0d4b5e19f2607782f28c5520d7321fc421
ivanaazuka <ivana.situmorang@amarbank.co.id>
2021-08-14 13:05:28 +0700

    init project

---
+++ app/src/main/java/com/azuka/aplikasiujian/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.azuka.aplikasiujian.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Created by ivanaazuka on 8/14/21.
+ * Android Engineer
+ */
+
+abstract class BaseActivity : AppCompatActivity() {
+
+//    protected abstract val binding:
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+}
--- app/src/main/java/com/azuka/aplikasiujian/presentation/MainActivity.kt
+++ app/src/main/java/com/azuka/aplikasiujian/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.azuka.aplikasiujian
+package com.azuka.aplikasiujian.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.azuka.aplikasiujian.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
